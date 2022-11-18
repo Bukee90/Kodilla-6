f13297ff98466b6ccd2f0b791f5a6375fb1cb328
Dawid Bogusz <dawid.bogusz90.kursy@gmail.com>
2022-11-18 13:58:26 +0100

    modiffy index.html and script.js add tags fucntion to script.js

--- js/script.js
+++ js/script.js
@@ -42,16 +42,16 @@ const titleClickHandler = function (event) {
 /* 6.4 */
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
 
-function generateTitleLinks() {
+function generateTitleLinks(customSelector = '') {
   // console.log('funckja', generateTitleLinks);
   /* remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
-
   /* for each article */
-  const articles = document.querySelectorAll(optArticleSelector);
+  const articles = document.querySelectorAll(optArticleSelector + customSelector);
   let html = '';
   for (let article of articles) {
   /* get the article id */
@@ -72,5 +72,83 @@ function generateTitleLinks() {
     link.addEventListener('click', titleClickHandler);
   // console.log(links);
   }
+  
 }
 generateTitleLinks();
+
+/* 7.2 */
+function generateTags(){
+  /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+  /* START LOOP: for every article: */
+  for (let article of articles) {
+  /* find tags wrapper */
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
+    // console.log('tagsWrapper', tagsWrapper);
+  /* make html variable with empty string */
+    let html = '';
+  /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+    // console.log(articleTags);
+  /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+  /* START LOOP: for each tag */
+    for (let tag of articleTagsArray){
+      // console.log(articleTagsArray);
+  /* generate HTML of the link */
+      const linkHTML = '<li><a href="#tag-' + tag + '"><span>' + tag + '</span></a></li>';
+      // console.log(linkHTML);
+  /* add generated code to html variable */
+      html = html + linkHTML + '';
+  /* END LOOP: for each tag */
+    }
+  /* insert HTML of all the links into the tags wrapper */
+  tagsWrapper.innerHTML = html;
+  /* END LOOP: for every article: */
+  }
+}
+generateTags();
+
+
+function tagClickHandler(event){
+  /* prevent default action for this event */
+  event.preventDefault();
+  /* make new constant named "clickedElement" and give it the value of "this" */
+  const clickedElement = this;
+  /* make a new constant "href" and read the attribute "href" of the clicked element */
+  const href = clickedElement.getAttribute('href');
+  /* make a new constant "tag" and extract tag from the "href" constant */
+  const tag = href.replace('#tag-', '');
+  /* find all tag links with class active */
+  const activeTags = document.querySelectorAll('a.active[href^="#tag-"]');
+  /* START LOOP: for each active tag link */
+  for (let activeTag of activeTags) {
+    /* remove class active */
+    activeTag.classList.remove('active');
+  /* END LOOP: for each active tag link */
+  }
+  /* find all tag links with "href" attribute equal to the "href" constant */
+  const tagHref = document.querySelectorAll('a[href="' + href + '"]');
+  /* START LOOP: for each found tag link */
+  for (let tagLink of tagHref) {
+    /* add class active */
+    tagLink.classList.add('active')
+  /* END LOOP: for each found tag link */
+  }
+  /* execute function "generateTitleLinks" with article selector as argument */
+  generateTitleLinks('[data-tags~="' + tag + '"]');
+}
+
+function addClickListenersToTags(){
+  /* find all links to tags */
+  const tagLinks = document.querySelectorAll('a[href^="#tag-"]');
+  /* START LOOP: for each link */
+    for (let tagLink of tagLinks) {
+  /* add tagClickHandler as event listener for that link */
+      tagLink.addEventListener('click', tagClickHandler);
+  /* END LOOP: for each link */
+  }
+}
+addClickListenersToTags();
+
+/* 7.2 - Autor*/
